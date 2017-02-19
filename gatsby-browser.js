b062ee12653f2116e4aba2a34cc3fcddee368f86
Jacob Bolda <jbolda@gmail.com>
2017-02-18 22:54:49 -0600

    replace react-ga with google tag manager

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,6 @@
-import ReactGA from 'react-ga'
-import {config} from 'config'
-
-ReactGA.initialize(config.googleAnalyticsId);
-
-exports.onRouteUpdate = (state, page, pages) => {
-  ReactGA.pageview(state.pathname);
-};
-
 exports.modifyRoutes = routes => {
     routes.pages.forEach(route => route.path = route.path.replace(/\/$/, ''));
     routes.childRoutes = recursiveRouteMod(routes.childRoutes);
-    console.log(routes)
     return routes;
 };
 
--- html.js
+++ html.js
@@ -28,8 +28,19 @@ module.exports = React.createClass({
               { head.meta.toComponent() }
               { font }
               { css }
+              <!-- Google Tag Manager -->
+              <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5BSQFD');</script>
+              <!-- End Google Tag Manager -->
             </head>
             <body>
+              <!-- Google Tag Manager (noscript) -->
+              <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BSQFD"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+              <!-- End Google Tag Manager (noscript) -->
               <div id="react-mount" dangerouslySetInnerHTML={ {    __html: this.props.body} } />
               <script async src={ prefixLink(`/bundle.js?t=${BUILD_TIME}`) } />
             </body>
