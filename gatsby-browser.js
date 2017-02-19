0852f72793968ae3e145de0a981789d987bf4eb7
Jacob Bolda <jbolda@gmail.com>
2017-02-18 22:36:41 -0600

    remove trailing slash from urls

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,20 @@ ReactGA.initialize(config.googleAnalyticsId);
 exports.onRouteUpdate = (state, page, pages) => {
   ReactGA.pageview(state.pathname);
 };
+
+exports.modifyRoutes = routes => {
+    routes.pages.forEach(route => route.path = route.path.replace(/\/$/, ''));
+    routes.childRoutes = recursiveRouteMod(routes.childRoutes);
+    console.log(routes)
+    return routes;
+};
+
+let recursiveRouteMod = (childRoutes) => {
+    childRoutes.forEach(route => {
+        route.path = route.path.replace(/\/$/, '')
+        if (route.childRoutes != undefined) {
+            recursiveRouteMod(route.childRoutes)
+        }
+    });
+    return childRoutes;
+}
--- pages/articles/2016-11-04-gmail-archived-and-unread/index.md
+++ pages/articles/2016-11-04-gmail-archived-and-unread/index.md
@@ -2,7 +2,7 @@
 title: Gmail Archive and Unread
 written: "2016-11-04"
 layout: post
-path: "/gmail-archive-and-unread/"
+path: "/gmail-archive-and-unread"
 category: "productivity"
 description: "Fix the OCD by marking archived emails from unread to read in Gmail."
 ---
