f91be91bee52f0bee4dd205b7b7bb3faae1b8983
Jacob Bolda <jbolda@gmail.com>
2017-02-20 19:34:22 -0600

    fix google tag manager

--- html.js
+++ html.js
@@ -18,17 +18,11 @@ module.exports = React.createClass({
             css = <style dangerouslySetInnerHTML={ {    __html: require('!raw!./public/styles.css')} } />
         }
 
-        const gaHead = `<!-- Google Tag Manager -->
-              <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        const gaHead = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5BSQFD');</script>
-              <!-- End Google Tag Manager -->`;
-        const gaBody = `<!-- Google Tag Manager (noscript) -->
-              <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BSQFD"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-              <!-- End Google Tag Manager (noscript) -->`;
+              })(window,document,'script','dataLayer','GTM-5BSQFD');`;
 
         return (
             <html lang="en">
@@ -36,14 +30,25 @@ module.exports = React.createClass({
               <meta charSet="utf-8" />
               <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
               <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
+              {/* Google Tag Manager */}
+              <script dangerouslySetInnerHTML={{__html: gaHead }}></script>
+              {/* End Google Tag Manager */}
               { head.title.toComponent() }
               { head.meta.toComponent() }
               { font }
               { css }
-              { gaHead }
             </head>
             <body>
-              { gaBody }
+              {/* Google Tag Manager (noscript) */}
+              <noscript>
+                <iframe
+                  src={'https://www.googletagmanager.com/ns.html?id=GTM-5BSQFD'}
+                  height={0}
+                  width={0}
+                  style={{ display: 'none', visibility: 'hidden'}}>
+                </iframe>
+              </noscript>
+              {/* End Google Tag Manager (noscript) */}
               <div id="react-mount" dangerouslySetInnerHTML={ {    __html: this.props.body} } />
               <script async src={ prefixLink(`/bundle.js?t=${BUILD_TIME}`) } />
             </body>
