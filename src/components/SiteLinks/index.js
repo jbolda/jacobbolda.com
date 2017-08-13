a8e522a03bbf39931f3655e7e01e879451cee5ec
Jacob Bolda <jbolda@gmail.com>
2017-08-12 23:00:21 -0500

    idk why this errors on build

--- src/components/SiteLinks/index.js
+++ src/components/SiteLinks/index.js
@@ -5,28 +5,15 @@ import '../../static/fonts/fontawesome/style.css';
 
 class SiteLinks extends React.Component {
   render() {
-      const {siteMetadata} = this.props;
+      const siteMetadata = this.props.siteMetadata;
 
       return (
           <div className='blog-social'>
             <ul>
               <li>
-                <a href={'mailto:'+siteMetadata.siteEmailUrl }><i className='fa fa-envelope-o' /> { siteMetadata.siteEmailPretty }</a>
-              </li>
-              <li>
-                <a href={ siteMetadata.siteLinkedInUrl }><i className='fa fa-linkedin-square' /> { siteMetadata.siteLinkedInPretty }</a>
-              </li>
-              <li>
-                <a href={ siteMetadata.siteTwitterUrl }><i className='fa fa-twitter' /> { siteMetadata.siteTwitterPretty }</a>
-              </li>
-              <li>
-                <a href={ siteMetadata.siteGithubUrl }><i className='fa fa-github-alt' /> { siteMetadata.siteGithubPretty }</a>
-              </li>
-              <li>
-                <a href={ siteMetadata.siteKeybaseUrl }><i className='fa fa-key' /> { siteMetadata.siteKeybasePretty }</a>
-              </li>
-              <li>
-                <a href={ siteMetadata.sitePhotoUrl }><i className='fa fa-camera' /> { siteMetadata.sitePhotoPretty }</a>
+                <a href={`${siteMetadata.siteEmailUrl}`}>
+                  <i className='fa fa-envelope-o' /> siteEmailPretty
+                </a>
               </li>
             </ul>
           </div>
@@ -35,3 +22,36 @@ class SiteLinks extends React.Component {
 }
 
 export default SiteLinks;
+
+/*
+              <li>
+                <a href={siteMetadata.siteEmailUrl}>
+                  <i className='fa fa-envelope-o' /> {siteMetadata.siteEmailPretty}
+                </a>
+              </li>
+              <li>
+                <a href={ siteMetadata.siteLinkedInUrl }>
+                  <i className='fa fa-linkedin-square' /> {siteMetadata.siteLinkedInPretty}
+                </a>
+              </li>
+              <li>
+                <a href={ siteMetadata.siteTwitterUrl }>
+                  <i className='fa fa-twitter' /> {siteMetadata.siteTwitterPretty}
+                </a>
+              </li>
+              <li>
+                <a href={ siteMetadata.siteGithubUrl }>
+                  <i className='fa fa-github-alt' /> {siteMetadata.siteGithubPretty}
+                </a>
+              </li>
+              <li>
+                <a href={ siteMetadata.siteKeybaseUrl }>
+                  <i className='fa fa-key' /> {siteMetadata.siteKeybasePretty}
+                </a>
+              </li>
+              <li>
+                <a href={ siteMetadata.sitePhotoUrl }>
+                  <i className='fa fa-camera' /> {siteMetadata.sitePhotoPretty}
+                </a>
+              </li>
+*/
