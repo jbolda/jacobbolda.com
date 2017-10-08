82949330875e775df725f725051d88c52945cc34
Jacob Bolda <jbolda@gmail.com>
2017-10-08 17:43:01 -0500

    add Url to end of key on config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     siteTwitterPretty: "@jacob_bolda",
     siteGithubUrl: "https://github.com/jbolda",
     siteGithubPretty: "github.com/jbolda",
-    siteAngelList: "https://angel.co/jacobbolda",
+    siteAngelListUrl: "https://angel.co/jacobbolda",
     siteAngelListPretty: "angel.co/jacobbolda",
     siteKeybaseUrl: "https://keybase.io/jbolda",
     siteKeybasePretty: "keybase.io/jbolda",
