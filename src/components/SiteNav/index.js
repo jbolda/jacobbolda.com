afbf8bfb622473f628ae5b153a6c31d1f566259a
Jacob Bolda <jbolda@gmail.com>
2017-10-28 10:25:04 -0500

    hacky workaround because :export doesn't work in sass

--- src/components/SiteNav/index.js
+++ src/components/SiteNav/index.js
@@ -1,22 +1,21 @@
 import React from "react";
 import Link from "gatsby-link";
-import {P5, P2} from "../../static/css/base.scss";
+import styles from "../../static/css/palette.json";
 
 class SiteNav extends React.Component {
   render() {
-    const { location } = this.props
-    console.log(location)
+
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
           <Link
             to="/"
             className="navbar-item"
-            style={location.pathname === '/' ? {backgroundColor: P5} : {}}
+            style={this.props.location.pathname == '/' ? {backgroundColor: styles.colors.P5} : {}}
             >
             <span
               className="title"
-              style={location.pathname === '/' ? {color: P2} : {color: P5}}
+              style={this.props.location.pathname == '/' ? {color: styles.colors.P2} : {color: styles.colors.P5}}
               >
               B
             </span>
@@ -24,21 +23,21 @@ class SiteNav extends React.Component {
           <Link
             to="/"
             className="navbar-item"
-            style={location.pathname === '/' ? {backgroundColor: P5, color: P2} : {}}
+            style={this.props.location.pathname == '/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
             >
             Articles
           </Link>
           <Link
             to="/about/"
             className="navbar-item"
-            style={location.pathname === '/about/' ? {backgroundColor: P5, color: P2} : {}}
+            style={this.props.location.pathname == '/about/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
             >
             About
           </Link>
           <Link
             to="/contact/"
             className="navbar-item"
-            style={location.pathname === '/contact/' ? {backgroundColor: P5, color: P2} : {}}
+            style={this.props.location.pathname == '/contact/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
             >
             Contact
           </Link>
