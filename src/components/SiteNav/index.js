76521e29739c3627f074316a8a042da792c4104a
Jacob Bolda <jbolda@gmail.com>
2017-11-20 00:10:09 -0600

    fix navbar to top

--- src/components/SiteNav/index.js
+++ src/components/SiteNav/index.js
@@ -6,7 +6,7 @@ class SiteNav extends React.Component {
   render() {
 
     return (
-      <nav className="navbar" role="navigation" aria-label="main navigation">
+      <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
           <Link
             to="/"
