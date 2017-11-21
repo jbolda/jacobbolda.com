2486398ebd4f90e61497a4080c6130d8397205e9
Jacob Bolda <jbolda@gmail.com>
2017-11-20 23:36:30 -0600

    add hero image to one blog post, not styled

--- src/templates/cfBlogPost.js
+++ src/templates/cfBlogPost.js
@@ -1,13 +1,22 @@
 import React from "react"
 import BlogPostChrome from "../components/BlogPostChrome"
+import Img from 'gatsby-image'
 
 class cfBlogPost extends React.Component {
   render() {
-    const frontmatter = this.props.data.contentfulBlogPost
+    console.log(this)
+    const frontmatter = this.props.data.post
     const {html} = frontmatter.content.childMarkdownRemark
 
     return (
       <BlogPostChrome {...this.props.data.contentfulBlogPost}>
+        <section className="hero">
+          <div className="hero-body">
+            <div className="container">
+              <Img sizes={this.props.data.hero.childImageSharp.sizes} />
+            </div>
+          </div>
+        </section>
         <h1 className="title is-1">{frontmatter.title}</h1>
         <div className="content" dangerouslySetInnerHTML={{ __html: html }} />
       </BlogPostChrome>
@@ -19,7 +28,7 @@ export default cfBlogPost
 
 export const pageQuery = graphql`
   query cfBlogPostByID($id: String!) {
-    contentfulBlogPost(id: { eq: $id }) {
+    post: contentfulBlogPost(id: { eq: $id }) {
       title
       content {
         childMarkdownRemark {
@@ -27,6 +36,15 @@ export const pageQuery = graphql`
         }
       }
       ...cfBlogPost
+    }
+    hero: file(relativePath: {eq: "external/hero-images/on-las-vegas.jpg"}) {
+      childImageSharp {
+        # Specify the image processing steps right in the query
+        # Makes it trivial to update as your page's design changes.
+        sizes {
+          ...GatsbyImageSharpSizes_tracedSVG
+        }
+      }
     }
   }
 `
