40882652bff61e31f2e59b3773c85a1284ad8d55
Jacob Bolda <me@jacobbolda.com>
2018-04-06 21:51:04 -0500

    switch photo into top hero, add contact info in second

--- plugins/gatsby-theme-bulma-homepage/Hero/HeroLayout.js
+++ plugins/gatsby-theme-bulma-homepage/Hero/HeroLayout.js
@@ -2,6 +2,7 @@ import React from "react"
 import Link from "gatsby-link"
 import Img from 'gatsby-image'
 import SimpleNav from "../../gatsby-theme-bulma-layout/Simple/SimpleNav"
+import SiteLinks from "../shared-components/SiteLinks"
 
 class InsetLayout extends React.Component {
   render() {
@@ -11,26 +12,31 @@ class InsetLayout extends React.Component {
       <SimpleNav sitemetadata={siteMetadata} location={this.props.location}>
         <section className="hero is-large is-secondary is-bold">
           <div className="hero-body">
-            <div className="container">
-              <h3 className="subtitle">
-                Hi, I am
-              </h3>
-              <h1 className="title">
-                <Link to={`/`}>
-                  Jacob Bolda
-                </Link>
-              </h1>
-              <h2 className="subtitle">
-                Structural Engineer
-              </h2>
+            <div className="columns">
+              <div className="column is-one-third">
+                <Img className="image box" Tag="figure" sizes={this.props.data.file.childImageSharp.sizes} />
+              </div> 
+              <div className="column">
+                <h3 className="subtitle">
+                  Hi, I am
+                </h3>
+                <h1 className="title">
+                  <Link to={`/`}>
+                    Jacob Bolda
+                  </Link>
+                </h1>
+                <h2 className="subtitle">
+                  Structural Engineer
+                </h2>
+              </div>
             </div>
           </div>
         </section>
         <section className="hero is-thirdary is-medium">
           <div className="hero-body">
             <div className="columns">
-              <div className="column is-one-third">
-                <Img className="image box" Tag="figure" sizes={this.props.data.file.childImageSharp.sizes} />
+              <div className="column is-one-quarter">
+                <SiteLinks {...this.props} />
               </div>
               <div className="column">
                 <div className="content">
