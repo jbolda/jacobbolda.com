5ccc648a567ab00d453175ad62ab1d2689e9380a
Jacob Bolda <me@jacobbolda.com>
2018-06-20 07:31:31 -0500

    clean up and add AECC

--- src/mainPages/_professional-engagements.js
+++ src/mainPages/_professional-engagements.js
@@ -7,17 +7,18 @@ const professionalEngagements = props =>
     <div className="tile is-vertical is-8">
       <div className="tile">
         <div className="tile is-parent is-vertical">
-          <ChildTile Child={PodcastTile} />
+          <ChildTile Child={PodcastSTEMonFire} />
           <ChildTile Child={REUTile} />
         </div>
         <div className="tile is-parent is-vertical">
-          <ChildTile Child={MastersThesis} />
-          <ChildTile Child={MitchellLofts} />
+          <ChildTile Child={AECCollective} />
           <ChildTile Child={ProgrammingKnowledge} />
+          <ChildTile Child={MitchellLofts} />
         </div>
       </div>
     </div>
-    <div className="tile is-parent">
+    <div className="tile is-parent is-vertical">
+      <ChildTile Child={MastersThesis} />
       <ChildTile Child={EngineersWithoutBorders} />
     </div>
   </div>
@@ -29,7 +30,7 @@ const ChildTile = ({props, Child}) =>
     <Child />
   </article>
 
-const PodcastTile = props =>
+const PodcastSTEMonFire = props =>
   <div>
     <TileTitle
       text="STEM on Fire Guest"
@@ -43,15 +44,34 @@ const PodcastTile = props =>
         {' '}through the curriculum and additional strategies to guide students towards a successful career.
       </p>
       <p>
-        I talk about how the AEC Industry is all connected, and how Structural Engineering fits in.
+        I talk about how the AEC Industry is all connected. Architectural Engineering will cover all the pieces that go into actually building the building. Structural engineering, at it’s core, revolves around making sure the building and/or structure stays up.
+      </p>
+      <p>
+        To get through college, I express that you need to figure out during high school how you learn and also make sure to nurture your network and give value to those around you without expecting anything in return.
+      </p>
+      <p>
+        I recommended Asana for a to-do list and the book “Never Split the Difference” by Chris Voss.
       </p>
     </div>
+  </div>
+
+const AECCollective = props =>
+  <div>
+    <TileTitle
+      text="AEC Collective"
+      url="https://www.aeccollective.com/"
+      />
+    <TileSub text="A Community Serving the Architectural, Engineering and Construction Industry" />
+    <p>
+      I run a community called the AEC Collective.
+      The AEC Collective is a community for the Architecture, Engineering, and Construction. It is uses the Discord program which is a free voice, video and text chat app that you can access via PC, browser, or mobile phone. We look to help mentor those just starting in the industry, and provide a great place for networking with your peers around the world. We can all benefit from understanding each related niche better, but also understanding our own niche outside of our geographic region.
+    </p>
   </div>
 
 const MitchellLofts = props =>
   <div>
     <TileTitle
-      text="Mitchell Street Market Lofts"
+      text="Mitchell Street Market Lofts Preliminary Design"
       url="http://www.impactseven.org/portfolio-items/mitchell-street-market-lofts/"
       />
     <TileSub text="New Construction Affordable Housing Development" />
@@ -78,7 +98,7 @@ const MastersThesis = props =>
     <TileSub text="Thesis Circa 2011" />
     <div className="content">
       <p>
-        The purpose of this capstone design project report is to discuss the behavior of a hooked bar in concrete carrying a tension force. The cover and bonded length are varied to observe the effect on the load distribution between the hook portion and the bonded length portion of a hooked bar. Each specimen had 1 or 2 inches of cover. Each specimen had a bonded length of 8, 12, or 16 inches. Straight rebar was also tested to provide a control and comparison to the hooked bars with similar cover and bond length variables.
+        The purpose of my capstone design project report was to discuss the behavior of a hooked bar in concrete carrying a tension force. The cover and bonded length were varied to observe the effect on the load distribution between the hook portion and the bonded length portion of a hooked bar. Each specimen had 1 or 2 inches of cover. Each specimen had a bonded length of 8, 12, or 16 inches. Straight rebar was also tested to provide a control and comparison to the hooked bars with similar cover and bond length variables. The abstract and full text can be accessed by clicking the header.
       </p>
     </div>
   </div>
@@ -92,8 +112,8 @@ const ProgrammingKnowledge = props =>
     <TileSub text="Since the '90s" />
     <div className="content">
       <p>
-        I have been known to sling some code. My experience crosses a unique set of languages, but all with the common goal of solving a specific problem. Programming and code are tools that have great power when wielded effectively. More recently my interest has been in JavaScript, and I had worked on some open source code (linked).
-        I am also an expert in Excel and have crafted some <a href="https://stackoverflow.com/questions/14614923/excel-formula-identifying-number-of-date-ranges-within-a-range/14616697#14616697" target="_blank">involved formulas and code under unique constraints</a>.
+        My experience crosses a unique set of languages, but all with the common goal of solving a specific problem. Programming and code are tools that have great power when wielded effectively. I consider my primary languages JavaScript, Python and VBA (see Excel), but I have additionally worked with Go, QBASIC, Fortran, LISP, SQL, Bash/Shell/Powershell, PHP, HTML, CSS, and more. More recently my interest has been in JavaScript, and I have working on open source code expounded upon in the linked article. My overall focus has been in data, manipulating it, and then visualizing it (see the love for JavaScript and Python).
+        I am also an expert in Excel and have crafted some <a href="https://stackoverflow.com/questions/14614923/excel-formula-identifying-number-of-date-ranges-within-a-range/14616697#14616697" target="_blank">involved formulas and code under unique constraints</a> as well as many template spreadsheets still in use at past and present professional engagements.
       </p>
     </div>
   </div>
@@ -114,13 +134,13 @@ const EngineersWithoutBorders = props =>
     </figure>
     <div className="content">
       <p>
-        In March 2010, a team of EWB-MSOE students and professionals completed a vehicular bridge in the rural village of Tres Cruces, Guatemala. Similar to our other bridges in Joyabaj, this one provides reliable year-round access for about 4,000 people in Tres Cruces and the surrounding countryside to reach the central town of Joyabaj for education, commerce, and healthcare. It should also open the doorway to further development of Tres Cruces and the surrounding region.
+        In March 2010, our team of EWB-MSOE students and professionals completed a vehicular bridge in the rural village of Tres Cruces, Guatemala. Similar to our other bridges in Joyabaj, this one provides reliable year-round access for about 4,000 people in Tres Cruces and the surrounding countryside to reach the central town of Joyabaj for education, commerce, and healthcare. It should also open the doorway to further development of Tres Cruces and the surrounding region.
       </p>
       <p>
         This project required two implementation trips. The professional partners on the first trip, conducted in January 2010, excavated and poured the bridge foundations and abutments. The second trip, composed of students and professional/faculty mentors, then completed the superstructure and retaining walls on the sides of the bridge. In preparation for these implementation trips, the professionals and students worked over the summer and fall of 2009 to produce a technical design for the bridge and a plan for constructing it with the close help of the municipal government of Joyabaj and the community leadership of Tres Cruces. The members of the local community, having an eagerness to invest in this improvement to their lives, willingly provided much of the labor for both implementation trips, along with on-site housing for EWB personnell. 
       </p>
       <p>
-        Special thanks to Doug Stahl, Mike Paddock, Max Schmiege, Rob Merkel, Steve Berg, Brian Sides, Heather Cleveland, Scott Solverson, and Carrie Bristol-Groll for their contributions as professional mentors for this project. We would also like to thank (again) our in-country contact, Mike Shawcross, for providing the link between us and the community of Tres Cruces.
+        I was able to travel on the second implementation trip to construct the superstructure and wing walls as seen in the embeded video that I created.
       </p>
     </div>
   </div>
@@ -137,17 +157,17 @@ const REUTile = props =>
     </figure>
     <div className="content">
       <p>
-        I research the applicability of creating a Revit model, running daylighting
+        I researched the applicability of creating a Revit model, running daylighting
         analyses and exporting the colored model to be 3D printed in color.
       </p>
       <p>
-        The purpose of this research is to explore the feasibility of performing energy analyses early in the design stage of building. Currently, energy analyses are not performed on a building until 80%-90% of the design decisions have been finalized. Once this point has been reached, passive solutions are hard to incorporate and designers depend on active measures to correct deficiencies in design. If energy modeling becomes a higher priority in designing a building, passive design solutions might easily be incorporated. Utilizing computer programs simplifies and expedites the energy modeling process. Energy modeling was examined to determine the ease of use and feasibility of using such a device to help designers understand the priorities behind passive design. This research focuses primarily on illumination with respect to solar energy uses. Values obtained from an energy model making use of daylighting and solar energy to help illuminate the room can then be compared to the same room using primarily electric illumination. A visual, as well as a textual, analysis can be performed and used to help educate not only designers but the client. Using rapid prototyping, a striking visual model can be created to help convey the energy efficiencies as well as the architectural highlights of the building.
+        The purpose of the research was to explore the feasibility of performing energy analyses early in the design stage of building. This has become much more commonplace since my research. Utilizing computer programs simplifies and expedites the energy modeling process. Energy modeling was examined to determine the ease of use and feasibility of using such a device to help designers understand the priorities behind passive design. A visual, as well as a textual, analysis can be performed and used to help educate not only designers but the client. Using rapid prototyping, a striking visual model can be created to help convey the energy efficiencies as well as the architectural highlights of the building.
       </p>
     </div>
   </div>
 
 const TileTitle = ({text, url}) =>
-  <p className="title is-4">
+  <p className="title is-5 has-text-centered">
     {url ? <TileLink url={url} text={text} /> : text}
   </p>
 
@@ -160,6 +180,6 @@ const TileLink = ({url, text}) => {
 }
   
 const TileSub = ({text}) =>
-  <p className="subtitle">
+  <p className="subtitle is-6 has-text-centered">
     {text}
   </p>
