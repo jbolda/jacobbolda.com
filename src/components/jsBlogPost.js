3ea39a1686abd58e04d476d52f5e18fb40320c94
Jacob Bolda <me@jacobbolda.com>
2018-07-04 09:52:42 -0500

    add graphql tag to jsBlogPost

--- src/components/jsBlogPost.js
+++ src/components/jsBlogPost.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { graphql } from "gatsby";
 import SimpleChromeBridge from "../utils/SimpleChromeBridge";
 
 const JSBlogPost = ({ data, location, children }) => (
