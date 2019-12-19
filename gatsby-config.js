12dcc92a7ee8a574600d52f9ebd22ce71931337f
Jacob Bolda <me@jacobbolda.com>
2019-12-19 07:19:54 -0600

    add responsive iframe back into config

--- gatsby-config.js
+++ gatsby-config.js
@@ -113,7 +113,8 @@ module.exports = {
             options: {
               maxWidth: 590
             }
-          }
+          },
+          `gatsby-remark-responsive-iframe`
         ]
       }
     },
