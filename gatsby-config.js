c3fc84397df91105610337a228e9abdca8681f1f
Jacob Bolda <me@jacobbolda.com>
2020-02-20 21:43:55 -0600

    enable webp on gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,7 +114,8 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590
+              maxWidth: 590,
+              withWebp: true
             }
           },
           { resolve: `gatsby-remark-copy-linked-files` },
