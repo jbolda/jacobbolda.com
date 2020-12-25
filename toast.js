b798a6288f72c4e5674677c53584e207ef7a84c5
Jacob Bolda <me@jacobbolda.com>
2020-12-24 23:51:26 -0600

    switch mdx to meta

--- toast.js
+++ toast.js
@@ -3,8 +3,15 @@ import { sourceMdx } from "@toastdotdev/mdx";
 export const sourceData = async ({ setDataForSlug }) => {
   await sourceMdx({
     setDataForSlug,
-    directory: "./content",
+    directory: "./content/articles",
     slugPrefix: "/",
   });
+
+  await sourceMdx({
+    setDataForSlug,
+    directory: "./content/notes",
+    slugPrefix: "/",
+  });
+
   return;
 };
