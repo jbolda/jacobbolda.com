8c86184bf5ac5af6f49697191dc1d07411d57ca7
Jacob Bolda <me@jacobbolda.com>
2020-12-28 10:46:20 -0600

    hash in old homepage structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,39 @@
+/* @jsx h */
 import { h } from "preact";
+import { MDXProvider } from "@mdx-js/preact";
+import Landing from "./../../an-extra-boop-for-the-homepage/landing.js";
+import About from "./../../an-extra-boop-for-the-homepage/about.js";
+
+import STEMonFire from "./../../an-extra-boop-for-the-homepage/STE-Mon-Fire.js";
+import AECC from "./../../an-extra-boop-for-the-homepage/AEC-Collective.js";
+import Masters from "./../../an-extra-boop-for-the-homepage/masters-thesis.js";
+import Prog from "./../../an-extra-boop-for-the-homepage/programming.js";
+import EWB from "./../../an-extra-boop-for-the-homepage/EWB.js";
+import MitchellLofts from "./../../an-extra-boop-for-the-homepage/mitchell-street-market-lofts.js";
+import REU from "./../../an-extra-boop-for-the-homepage/reu.js";
 
 export default (props) => (
-  <div>
-    <h1>a marketing landing page</h1>
-  </div>
+  <MDXProvider>
+    <div>
+      <img src="/avatar.png" />
+      <Landing />
+    </div>
+    <About />
+    <div>
+      <STEMonFire />
+      <AECC />
+      <Masters />
+      <Prog />
+      <EWB />
+      <MitchellLofts />
+      <REU />
+    </div>
+    <div>
+      {props.articles.map((article) => (
+        <p>
+          <a href={article.meta.slug}>{article.meta.title}</a>
+        </p>
+      ))}
+    </div>
+  </MDXProvider>
 );
