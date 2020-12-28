fc0ade43b7f9eb7980a8ada6d2ae1f1de093be2a
Jacob Bolda <me@jacobbolda.com>
2020-12-28 10:44:34 -0600

    temp shift article components to src
    we need some toast additions to keep them adjacent without a bunch of effort, go the easy route for now

--- toast.js
+++ toast.js
@@ -1,16 +1,26 @@
 import { sourceMdx } from "@toastdotdev/mdx";
 
 export const sourceData = async ({ setDataForSlug }) => {
-  await sourceMdx({
+  const articles = await sourceMdx({
     setDataForSlug,
     directory: "./content/articles",
     slugPrefix: "/",
   });
 
-  await sourceMdx({
+  const notes = await sourceMdx({
     setDataForSlug,
     directory: "./content/notes",
     slugPrefix: "/",
+  });
+
+  const homepage = await sourceMdx({
+    setDataForSlug,
+    directory: "./content/homepage",
+    slugPrefix: "/an-extra-boop-for-the-homepage/",
+  });
+
+  await setDataForSlug("/", {
+    data: { articles: [...articles, ...notes] },
   });
 
   return;
