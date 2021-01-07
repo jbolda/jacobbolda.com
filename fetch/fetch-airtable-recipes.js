cf0049a18d39f39cc4ccb6ca57b63383414f0f2b
Jacob Bolda <me@jacobbolda.com>
2021-01-07 01:41:22 -0600

    add airtable recipe fetching and main page

---
+++ fetch/fetch-airtable-recipes.js
@@ -0,0 +1,54 @@
+import Airtable from "airtable";
+
+export const sourceAirtableRecipes = async ({ tables }) => {
+  try {
+    // hoist api so we can use in scope outside of this block
+    var api = await new Airtable({
+      apiKey: process.env.AIRTABLE_API_KEY,
+    });
+  } catch (e) {
+    // airtable uses `assert` which doesn't exit the process
+    console.warn("\nAPI key is required to connect to Airtable");
+    return;
+  }
+
+  console.time(`\nfetch all Airtable rows from ${tables.length} tables`);
+
+  let queue = [];
+  tables.forEach((tableOptions) => {
+    let base = api.base(tableOptions.baseId);
+
+    let table = base(tableOptions.tableName);
+
+    let view = tableOptions.tableView || "";
+
+    let query = table.select({
+      view: view,
+    });
+
+    // query.all() returns a promise, pass an array for each table with
+    // both our promise and the queryName and then map reduce at the
+    // final promise resolution to get queryName onto each row
+    queue.push(Promise.all([query.all(), tableOptions.queryName]));
+  });
+
+  // queue has array of promises and when resolved becomes nested arrays
+  // we flatten the array to return all rows from all tables after mapping
+  // the queryName to each row
+  return Promise.all(queue)
+    .then((all) => {
+      const reduced = all.reduce((nested, query) => {
+        const rows = query[0];
+        const queryName = query[1];
+        nested[queryName] = rows.map((record) => record.fields);
+        return nested;
+      }, {});
+
+      console.timeEnd(`\nfetch all Airtable rows from ${tables.length} tables`);
+      return reduced;
+    })
+    .catch((e) => {
+      throw e;
+      return;
+    });
+};
--- toast.js
+++ toast.js
@@ -1,6 +1,9 @@
 import { sourceMdx } from "@toastdotdev/mdx";
+import { sourceAirtableRecipes } from "./fetch/fetch-airtable-recipes.js";
 
 export const sourceData = async ({ setDataForSlug }) => {
+  const { Recipes: recipes } = await sourceAirtableRecipes({ tables });
+
   const articles = await sourceMdx({
     setDataForSlug,
     directory: "./content/articles",
@@ -33,5 +36,30 @@ export const sourceData = async ({ setDataForSlug }) => {
     data: { articles: [...articles, ...notes], pageType: "page" },
   });
 
+  await setDataForSlug("/recipes", {
+    data: { recipes, pageType: "page" },
+  });
+
   return;
 };
+
+const tables = [
+  {
+    baseId: `appcL6Jdj7ZrhTg4q`,
+    tableName: `Recipes`,
+    tableView: `List`,
+    queryName: `Recipes`,
+  },
+  {
+    baseId: `appcL6Jdj7ZrhTg4q`,
+    tableName: `Cooking Method`,
+    tableView: `Main View`,
+    queryName: `Cooking Method`,
+  },
+  {
+    baseId: `appcL6Jdj7ZrhTg4q`,
+    tableName: `Style`,
+    tableView: `Main View`,
+    queryName: `Style`,
+  },
+];
