63409fb859d2c828b882013e1c6d5aa2465efbdb
Jacob Bolda <me@jacobbolda.com>
2021-01-11 11:07:40 -0600

    fix esinstall for other packages

--- .bin/box.js
+++ .bin/box.js
@@ -1,26 +1,43 @@
 #!/usr/bin/env node
-import {install, printStats} from 'esinstall'
+import { install, printStats } from "esinstall";
+
+const specs = [
+  "preact",
+  "preact/hooks",
+  "@mdx-js/preact",
+  "react-helmet",
+  "preact/compat",
+];
+
+const options = {
+  alias: {
+    react: "preact/compat",
+  },
+};
 
 // esinstall doesn't let us quiet the output while it runs
 // so we kinda do that here.
 const logger = {
   debug() {},
   warn(...args) {
-    console.warn(...args)
+    console.warn(...args);
   },
   error(...args) {
-    console.error(...args)
-  }
-}
+    console.error(...args);
+  },
+};
 
 async function main() {
-  const {success, stats} = await install(['preact', 'react-helmet'], {
-    dest: './public/web_modules',
-    logger
-  })
-  if (success) {
-    console.log(stats)
-  }
+  const { success, stats } = await install(specs, {
+    dest: "./public/web_modules",
+    logger,
+    ...options,
+  });
+  console.log(stats);
 }
 
-main()
+try {
+  main();
+} catch (e) {
+  throw e;
+}
