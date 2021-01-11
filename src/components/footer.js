fc30a65defa0b3517995edfb4377fe47971b3f5e
Jacob Bolda <me@jacobbolda.com>
2021-01-11 14:25:43 -0600

    update dark mode toggle svg

--- src/components/footer.js
+++ src/components/footer.js
@@ -119,13 +119,16 @@ const Toggle = (props) => {
           } absolute inset-0 h-full w-full flex items-center justify-center transition-opacity`}
           aria-hidden="true"
         >
-          <svg class="h-3 w-3 text-gray-400" fill="none" viewBox="0 0 12 12">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-3 w-3 text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
             <path
-              d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
-              stroke="currentColor"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
+              fillRule="evenodd"
+              d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+              clipRule="evenodd"
             />
           </svg>
         </span>
@@ -138,11 +141,18 @@ const Toggle = (props) => {
           aria-hidden="true"
         >
           <svg
+            xmlns="http://www.w3.org/2000/svg"
             class="h-3 w-3 text-indigo-600"
-            fill="currentColor"
-            viewBox="0 0 12 12"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
-            <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            />
           </svg>
         </span>
       </span>
