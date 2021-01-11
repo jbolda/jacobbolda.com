b8f5dc677390ab0c46bc882b12ad20c4ab0bad4d
Jacob Bolda <me@jacobbolda.com>
2021-01-11 01:54:39 -0600

    bootstrap in dark mode

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import { h } from "preact";
 
-export default (props) => (
+export default ({ toggle }) => (
   <footer class="bg-white">
     <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
       <nav
@@ -23,6 +23,10 @@ export default (props) => (
           >
             Recipes
           </a>
+        </div>
+
+        <div class="px-5 py-2">
+          <Toggle />
         </div>
       </nav>
       <div class="mt-8 flex justify-center space-x-6">
@@ -66,3 +70,59 @@ export default (props) => (
     </div>
   </footer>
 );
+
+const Toggle = (props) => {
+  const toggleAction = () => {
+    console.log("test", document.documentElement.classList.contains("dark"));
+    if (!document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.add("dark");
+      window.localStorage.setItem("nightwind-mode", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      window.localStorage.setItem("nightwind-mode", "light");
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      aria-pressed="false"
+      class="relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      onClick={() => toggleAction()}
+    >
+      {/* ^^ On: "bg-indigo-600", Off: "bg-gray-200" */}
+      <span class="sr-only">toggle dark mode</span>
+      {/* On: "translate-x-5", Off: "translate-x-0" */}
+      <span class="translate-x-0 relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200">
+        {/* On: "opacity-0 ease-out duration-100", Off: "opacity-100 ease-in duration-200" */}
+        <span
+          class="opacity-100 ease-in duration-200 absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
+          aria-hidden="true"
+        >
+          <svg class="h-3 w-3 text-gray-400" fill="none" viewBox="0 0 12 12">
+            <path
+              d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </span>
+        {/* On: "opacity-100 ease-in duration-200", Off: "opacity-0 ease-out duration-100" */}
+        <span
+          class="opacity-0 ease-out duration-100 absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
+          aria-hidden="true"
+        >
+          <svg
+            class="h-3 w-3 text-indigo-600"
+            fill="currentColor"
+            viewBox="0 0 12 12"
+          >
+            <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
+          </svg>
+        </span>
+      </span>
+    </button>
+  );
+};
