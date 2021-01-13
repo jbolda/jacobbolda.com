8746e2b2b3a33dc2a2923f05156e7795f6ff722a
Jacob Bolda <me@jacobbolda.com>
2021-01-13 13:31:38 -0600

    set max widths

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ import { ArticleWrap } from "./articles.js";
 export default (props) => (
   <div>
     <Hero />
-    <div class="relative pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+    <div class="relative max-w-prose mx-auto pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
       <About />
     </div>
     <Engagements>
