e3ace8493e4396d0620b01d84ea6cab8b9b64c99
Jacob Bolda <me@jacobbolda.com>
2021-06-07 23:34:36 -0500

    save component in case

--- src/components/unfurl.js
+++ src/components/unfurl.js
@@ -41,3 +41,72 @@ export default ({ title, subtitle }) => (
     />
   </Helmet>
 );
+
+/*
+<>
+  <Flex
+    sx={{
+      position: 'relative',
+      background: "linear-gradient(#52777d, #192c3b)",
+      color: 'white'
+    }}
+  >
+    <Flex
+      sx={{
+        border: '5px solid',
+        position: 'relative',
+        width: '100%',
+        overflow: 'hidden',
+      }}
+    >
+      <Flex
+        sx={{
+          width: '50%',
+          flexDirection: 'column',
+          justifyContent: 'space-around',
+          p: 5,
+          pr: 0,
+        }}
+      >
+        <Link
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=block'
+          rel='stylesheet'
+        />
+        <Text
+          sx={{
+            lineHeight: 1.25,
+            fontFamily: 'Inter',
+            fontSize: 5,
+            fontWeight: 700,
+          }}
+        >
+          {query.title}
+        </Text>
+        <Text
+          sx={{
+            lineHeight: 1.5,
+            fontFamily: 'Inter',
+            fontSize: 3,
+            fontWeight: 400,
+          }}
+        >
+          {query.subtitle}
+        </Text>
+      </Flex>
+      <Box
+        sx={{
+          width: `calc(50%)`,
+          backgroundImage: `url(https://www.jacobbolda.com/avatar.png)`,
+          backgroundSize: 'cover',
+        }}
+      />
+    </Flex>
+  </Flex>
+</>
+
+query:
+{
+"subtitle":"lesser boop the subtitle"
+"title":"boop a title"
+}
+*/
