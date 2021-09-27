17cfa86eadd4a0ed1c84871c9ab4716275d84c64
Jacob Bolda <me@jacobbolda.com>
2021-09-27 18:25:38 -0500

    watch css as well

--- scripts/watch.js
+++ scripts/watch.js
@@ -8,7 +8,7 @@ import {
   createChannel,
 } from "effection";
 import chokidar from "chokidar";
-import { exec } from "@effection/process";
+import { exec, daemon } from "@effection/process";
 
 function* runBuild() {
   console.log("building...");
@@ -17,16 +17,27 @@ function* runBuild() {
     env: { ...process.env, SITE_FILE_CACHE: "true" },
   });
 
+  yield logOutput(child);
+  console.timeEnd("run build command");
+}
+
+function* logOutput(child) {
   yield child.stdout.forEach((data) => {
     process.stdout.write(data);
   });
   yield child.stderr.forEach((data) => {
     process.stderr.write(data);
   });
-  console.timeEnd("run build command");
 }
 
 main(function* start() {
+  console.time("watching css");
+  const watchCSS = yield daemon("npm run build:css:watch", {
+    env: { ...process.env, TAILWIND_MODE: "watch" },
+  });
+  yield watchCSS.stdout.filter((chunk) => chunk.includes("postcss")).expect();
+  console.timeEnd("watching css");
+
   console.time("started file watching");
   let watcher = chokidar.watch(".", {
     ignored: "{node_modules,.tmp,public}",
@@ -38,6 +49,7 @@ main(function* start() {
 
   yield once(watcher, "ready");
   console.timeEnd("started file watching");
+
   yield runBuild();
 
   let fileChanges = createChannel();
