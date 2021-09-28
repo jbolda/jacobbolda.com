71c3f53e2d7e7830be933112d815a7e3f081d617
Jacob Bolda <me@jacobbolda.com>
2021-09-27 23:11:25 -0500

    normalize margins on text/headings (#226)

--- src/components/wrapperArticle.js
+++ src/components/wrapperArticle.js
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet";
 import Unfurl from "./unfurl.js";
 
 export default (props) => (
-  <div class="relative py-16 overflow-hidden flex-grow">
+  <div class="relative py-16 flex-grow">
     <Helmet>
       <title>{props.children.props.title}</title>
       <meta property="og:type" content="article" />
@@ -12,110 +12,8 @@ export default (props) => (
       title={props.children.props.title}
       subtitle={props.children.props.description}
     />
-    <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
-      <div class="relative h-full text-lg max-w-mx-auto" aria-hidden="true">
-        <svg
-          class="absolute top-12 left-full transform translate-x-32"
-          width={404}
-          height={384}
-          fill="none"
-          viewBox="0 0 404 384"
-        >
-          <defs>
-            <pattern
-              id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
-              x={0}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits="userSpaceOnUse"
-            >
-              <rect
-                x={0}
-                y={0}
-                width={4}
-                height={4}
-                class="text-gray-200"
-                fill="currentColor"
-              />
-            </pattern>
-          </defs>
-          <rect
-            width={404}
-            height={384}
-            fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
-          />
-        </svg>
-        <svg
-          class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
-          width={404}
-          height={384}
-          fill="none"
-          viewBox="0 0 404 384"
-        >
-          <defs>
-            <pattern
-              id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-              x={0}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits="userSpaceOnUse"
-            >
-              <rect
-                x={0}
-                y={0}
-                width={4}
-                height={4}
-                class="text-gray-200"
-                fill="currentColor"
-              />
-            </pattern>
-          </defs>
-          <rect
-            width={404}
-            height={384}
-            fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
-          />
-        </svg>
-        <svg
-          class="absolute bottom-12 left-full transform translate-x-32"
-          width={404}
-          height={384}
-          fill="none"
-          viewBox="0 0 404 384"
-        >
-          <defs>
-            <pattern
-              id="d3eb07ae-5182-43e6-857d-35c643af9034"
-              x={0}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits="userSpaceOnUse"
-            >
-              <rect
-                x={0}
-                y={0}
-                width={4}
-                height={4}
-                class="text-gray-200"
-                fill="currentColor"
-              />
-            </pattern>
-          </defs>
-          <rect
-            width={404}
-            height={384}
-            fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
-          />
-        </svg>
-      </div>
-    </div>
-    <div class="relative">
-      <article class="mt-6 text-gray-500 mx-auto px-2">
-        {props.children}
-      </article>
-    </div>
+    <article class="mt-6 text-gray-500 grid grid-col-1 gap-y-3 justify-items-center">
+      {props.children}
+    </article>
   </div>
 );
