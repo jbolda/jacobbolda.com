f769e1760ea0b5269a93295031fcb7859496d179
Jacob Bolda <me@jacobbolda.com>
2023-11-19 00:11:49 -0600

    fix receipe list styles

--- src/components/common/list.jsx
+++ src/components/common/list.jsx
@@ -5,7 +5,7 @@ export default ({ as = "li", classAdd = "", children, ...rest }) => {
   return (
     <Component
       className={`${
-        as === "ul" ? "list-disc " : "list-decimal "
+        as === "ul" ? "list-disc " : as === "ol" ? "list-decimal " : ""
       }list-inside text-xl md:text-lg lg:text-base text-primary-900 dark:text-primary-50 mx-auto px-2 mb-1 max-w-prose${
         classAdd.length > 0 ? ` ${classAdd}` : ""
       }`}
