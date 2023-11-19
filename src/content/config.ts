5bb9b40729ab891123873350e0a1c58ab38c98dc
Jacob Bolda <me@jacobbolda.com>
2023-11-18 23:55:29 -0600

    tweak frontmatter schema to bring back the seed graphic

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,11 +2,12 @@ import { defineCollection, reference, z } from "astro:content";
 
 const articleSchema = z.object({
   title: z.string(),
+  progress: z.enum(["seed", "growth", "article"]).optional(),
   written: z.union([z.date(), z.string()]),
   updated: z.union([z.date(), z.string()]).optional(),
   category: z.string().optional(),
   description: z.string(),
-  relatedPosts: z.array(reference("articles")).optional(),
+  relatedArticles: z.array(reference("articles")).optional(),
   keywords: z.string().array().optional(),
 });
 
